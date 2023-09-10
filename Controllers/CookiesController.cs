81551fad1f5ac9547338ccdcba251199fc060df9
tdk-cm <cme@td-k.dk>
2023-09-10 20:33:07 +0200

    Add project files.

---
+++ Controllers/CookiesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace H3_MilkAndCookies.Controllers
+{
+    public class CookiesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("SetFavoriteMilkshake")]
+        public IActionResult SetFavoriteMilkshake(string favoriteMilkshake)
+        {
+            CookieOptions co = new CookieOptions();
+                       
+
+            co.Expires = DateTime.Now.AddMinutes(5);
+            Response.Cookies.Append("favoriteMilkshake", favoriteMilkshake, co);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("GetFavoriteMilkshake")]
+        public IActionResult GetFavoriteFromCookies()
+        {
+            string? favCookie = Request.Cookies["favoriteMilkshake"];
+
+            if(favCookie is null)
+            {
+                return Ok("Unknown");
+            }
+            else
+            {
+                return Ok(favCookie);
+            }
+        }
+
+        [HttpGet]
+        [Route("DeleteMilkshakeCookie")]
+        public IActionResult DeleteMilkshakeCookie()
+        {
+            string? favCookie = Request.Cookies["favoriteMilkshake"];
+            if(favCookie is null)
+            {
+                return Ok("Cookie was already deleted");
+            }
+
+            CookieOptions co = new CookieOptions();
+
+            co.Expires = DateTime.Now.AddMinutes(-20);
+            Response.Cookies.Append("favoriteMilkshake", favCookie, co);
+            return Ok();
+        }
+    }
+}
